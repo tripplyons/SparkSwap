7dc8b0d9d7a0665e8e73b62676b371b396aa5c7c
mrmola <25235590+mrmola@users.noreply.github.com>
2023-11-04 15:55:51 -0400

    Janky horrid frontend

---
+++ sparkswap/app/components/WalletLogin.js
@@ -0,0 +1,21 @@
+import Button from '@mui/joy/Button';
+import Box from '@mui/joy/Box';
+
+export default function WalletLogin() {
+    return (
+        <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+            <Grid xs={8}>
+            <Box className="walletDisplay">
+                SparkSwap
+            </Box>
+            <Button className="walletButton" variant="solid" sx={{
+                width: '20',
+                display: 'flex',
+                flexDirection: 'row'
+            }}>
+                Bruh
+            </Button>
+        </Box>
+    );
+}
+  
